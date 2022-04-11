5392376ba463d56dfd1eb9a539d71749c2636a6e
NicoLas-44 <deutchman@gmail.com>
2022-04-11 10:28:11 +0200

    Nouvelle Piste
    Ajout d'une nouvelle piste

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -623,7 +623,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+//#define PIDTEMPBED //Explorer ici aussi
 
 //#define BED_LIMIT_SWITCHING //Explorer cette option.
 
