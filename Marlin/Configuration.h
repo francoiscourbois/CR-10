037b0a72d1ebb8f51b91b88796d620acd3cf14c9
NicoLas-44 <deutchman@gmail.com>
2022-06-07 09:15:55 +0200

    Ajustements BL-Touch
    Coordonnées BL-Touch
    Coordonnées Z_SAFE_HOMING

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1179,7 +1179,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 52, 9, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -52, -9, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1749,8 +1749,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
-  #define Z_SAFE_HOMING_Y_POINT Y_CENTER  // Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT 52 //X_CENTER  // X point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT 9 //Y_CENTER  // Y point for Z homing
 #endif
 
 // Homing speeds (mm/min)
