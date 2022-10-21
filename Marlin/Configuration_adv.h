1a0b76dd0806f1ca3f84181b15a5588faa39c03d
NicoLas-44 <deutchman@gmail.com>
2022-10-21 09:44:39 +0200

    Update Configuration_adv.h
    Mise à false de l'interpolate pour E0 en ligne 2801 et mise à 700ma en ligne 2797 du fichier configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2794,11 +2794,11 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT       750
+    #define E0_CURRENT       700
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
-    //#define E0_INTERPOLATE true
+    #define E0_INTERPOLATE false
     //#define E0_HOLD_MULTIPLIER 0.5
   #endif
 
